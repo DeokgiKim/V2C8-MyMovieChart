0d74b0f13fad3aa3ef4ab8f35c54c1102d8b6d66
DeokgiKim <easycalctech@gmail.com>
2018-09-29 17:55:01 +0900

    Initial Commit

---
+++ V2C8-MyMovieChart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  V2C8-MyMovieChart
+//
+//  Created by KDG on 29/09/2018.
+//  Copyright © 2018 deokgikim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
